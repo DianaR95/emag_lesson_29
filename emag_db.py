c84c5693145e3c41f0e53bb91b769cdb64f102e8
Diana Radulescu <radulescu.diana95@gmail.com>
2024-05-26 20:35:09 +0300

    created add_product function

--- emag_db.py
+++ emag_db.py
@@ -33,7 +33,22 @@ def read_products(config: dict, table: str= "emag.products"):
             return products_list
 
 
+def add_product(config: dict, table: str= "emag.products"):
+    with ps.connect(**config) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT name FROM {table}")
+            all_products = [product[0] for product in cursor.fetchall()]
+            new_prod = input("Add the new product: name store price \n")
+            new_prod = new_prod.split(" ")
+            if new_prod[0] not in all_products:
+                cursor.execute(f"INSERT INTO emag.products(name, store, price) VALUES ('{new_prod[0]}', '{new_prod[1]}', '{new_prod[2]}')")
+            else:
+                print("Produsul deja exista")
+
+
+
 if __name__ == '__main__':
     config = read_config()
     admins = read_admins(config)
     products = read_products(config)
+    new_prod = add_product(config)
